29c5278134773af328474df5cc9de66b4099011a
Michael Caisse <mcaisse@ciere.com>
2024-09-17 18:13:18 -0700

    support graphing N start_detached chains

--- gdb_util/gdb_graph.py
+++ gdb_util/gdb_graph.py
@@ -34,9 +34,47 @@ class DotColorMapper:
                 break
         
         return {'style': 'filled', 'fillcolor': mapped_color}
-        
 
-def test_graph(h):
+
+class DotColorMapperInt:
+    """
+    Signals have priority. The highest priority signal that is set
+    will dictate the color
+    """
+    signal_priority_map = (
+        ("set_error","orangered"),
+        ("set_stopped","lightgrey"),
+        ("set_value","dodgerblue"),
+        ("start","mediumseagreen"),
+        ("eval_predicate", "tan"))
+
+    def __init__(self, chain_name, frame):
+        self.chain_name = chain_name
+        self.frame = frame
+
+    def map(self, node_identifier):
+        if not node_identifier or not self.frame:
+            return {}
+
+        syms = get_symbols(self.chain_name, **node_identifier)
+
+        mapped_color = 'ghostwhite'
+        for (sig, color) in self.signal_priority_map:
+            sig_values = [s.get_value(self.frame) for s in syms if s.signal_name == sig]
+            if any(sig_values):
+                mapped_color = color
+                break
+        
+        return {'style': 'filled', 'fillcolor': mapped_color}
+
+
+def test_graph(handles):
     f = gdb.selected_frame()
-    h.handled.graph(DotGraphBuilder(color_mapper=DotColorMapper(h.chain_name, f)))[0].display()
-    
+    g = DotGraphBuilder(color_mapper=DotColorMapper("sender chains", f))
+
+    for h in handles:
+        sub = g.subgraph(h.chain_name, h.chain_name)
+        h.handled.graph(sub)
+        g.add_subgraph(sub)
+
+    g.display()
